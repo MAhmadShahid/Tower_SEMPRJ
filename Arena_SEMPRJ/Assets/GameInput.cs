6b37c51cb50c5148028ebcedd2fc080a0693baf0
Ahmad Shahid <71777028+MAhmadShahid@users.noreply.github.com>
2023-05-13 01:50:37 +0500

    Movement Prototype V0.2
    1. Learned theory of the new unity input system including
    > InputActions
    > ActionMaps
    Notes available in "LearningNotes" folder
    2. Made an InputActionMap.
    3. Made a wrapper class "GameInput" for the inputactionmap.

---
+++ Arena_SEMPRJ/Assets/GameInput.cs
@@ -0,0 +1,29 @@
+using Arena;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Arena
+{
+    public class GameInput : MonoBehaviour
+    {
+        [Tooltip("The InputActionAsset we will be using to manage actions")]
+        [SerializeField] private PlayerInputActions _playerInputActions;
+        void Awake()
+        {
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.Enable();
+
+        }
+
+
+        public Vector2 GetMovementVectorNormalized()
+        {
+            Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+            return inputVector.normalized;
+        }
+
+    }
+}
+
---
+++ Arena_SEMPRJ/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player")]
+    [Tooltip("Hieght of the player model")]
+    [SerializeField] private float _playerHeight = 1.0f;
+    [SerializeField] private float _moveSpeed = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
